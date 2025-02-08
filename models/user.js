c8d6356f494b5356fdae5c7e720834826ed31606
shivaraj-crest <shivaraj.p@crestskillserve.com>
2025-02-08 18:07:39 +0530

    first-commit

---
+++ models/user.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    mobile: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+    stripe_customer_id: DataTypes.STRING,
+    role: DataTypes.ENUM('admin', 'user'),
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true,
+  });
+  return User;
+};
